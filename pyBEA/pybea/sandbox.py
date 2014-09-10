ebbe096396f47118e8ed16092f9eb2652350f373
David R. Pugh <drobert.pugh@gmail.com>
2014-09-10 15:23:14 -0400

    Added access to the GetParameterList for a specified data set.

--- pyBEA/pybea/sandbox.py
+++ pyBEA/pybea/sandbox.py
@@ -16,3 +16,21 @@ class Data(object):
                      'ResultFormat': self.result_format}
         tmp_response = requests.get(url=self.base_url, params=tmp_query)
         return tmp_response
+
+    @property
+    def data_set_name(self):
+        return self._data_set_name
+
+    @property
+    def parameter_list(self):
+        tmp_query = {'UserID': self.api_key,
+                     'Method': 'GetParameterList',
+                     'DataSetName': self.data_set_name,
+                     'ResultFormat': self.result_format}
+        tmp_response = requests.get(url=self.base_url, params=tmp_query)
+        return tmp_response
+
+    @data_set_name.setter
+    def data_set_name(self, value):
+        self._data_set_name = value
+    
