42bed2b80a00721e0470192603a3e6248042446f
David R. Pugh <drobert.pugh@gmail.com>
2014-09-10 15:46:23 -0400

    Added minimal support for the GetData api call.

--- pyBEA/pybea/sandbox.py
+++ pyBEA/pybea/sandbox.py
@@ -10,6 +10,15 @@ class API(object):
         self.result_format = result_format
 
     @property
+    def data(self):
+        tmp_query = {'UserID': self.api_key,
+                     'Method': 'GetData',
+                     'DataSetName': self.data_set_name,
+                     'ResultFormat': self.result_format}
+        tmp_response = requests.get(url=self.base_url, params=tmp_query)
+        return tmp_response
+
+    @property
     def data_set_list(self):
         tmp_query = {'UserID': self.api_key,
                      'Method': 'GetDataSetList',
