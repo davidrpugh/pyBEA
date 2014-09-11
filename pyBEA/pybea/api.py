60b9d1252030daec010ec749e75aba8d2d2d4325
David R. Pugh <drobert.pugh@gmail.com>
2014-09-11 13:34:33 -0400

    Refactored to improve abstractions.

--- pyBEA/pybea/api.py
+++ pyBEA/pybea/api.py
@@ -1,3 +1,15 @@
+"""
+Want the request to have nicely formated request and results attributes that
+can easily be passed to the Pandas DataFrame constructor.
+
+Notes
+-----
+
+1. No longer clear to me that we need the subclasses for request.
+2. Use ElementTree for XML parsing.
+
+"""
+import json
 import requests
 
 
@@ -16,17 +28,57 @@ class Request(dict):
 
     def __setitem__(self, item, value):
         self._response = None
-        return super(Request, self).__setitem__(self, item, value)
+        return super(Request, self).__setitem__(item, value)
 
     def __delitem__(self, item):
         self._response = None
-        return super(Request, self).__delitem__(self, item)
+        return super(Request, self).__delitem__(item)
+
+    @property
+    def _json_request(self):
+        raw_json = self._load_json_content()
+        return raw_json['BEAAPI']['Request']
+
+    @property
+    def _json_results(self):
+        raw_json = self._load_json_content()
+        return raw_json['BEAAPI']['Results']
+
+    @property
+    def _xml_request(self):
+        raise NotImplementedError
+
+    @property
+    def _xml_results(self):
+        raise NotImplementedError
+
+    @property
+    def request(self):
+        if self['ResultFormat'] == 'JSON':
+            tmp_request = self._json_request
+        else:
+            tmp_request = self._xml_request
+        return tmp_request
 
     @property
     def response(self):
         if self._response is None:
             self._response = requests.get(url=self.base_url, params=self)
         return self._response
+
+    @property
+    def results(self):
+        if self['ResultFormat'] == 'JSON':
+            tmp_results = self._json_results
+        else:
+            tmp_results = self._xml_results
+        return tmp_results
+
+    def _load_json_content(self):
+        return json.loads(self.response.content)
+
+    def _load_xml_content(self):
+        raise NotImplementedError
 
 
 class DataSetListRequest(Request):
@@ -63,7 +115,26 @@ class ParameterValuesRequest(Request):
         super(ParameterValuesRequest, self).__init__(**required_params)
 
 
-class RegionalDataRequest(Request):
+class DataRequest(Request):
+
+    @property
+    def _json_data(self):
+        return self.results['Data']
+
+    @property
+    def _xml_data(self):
+        raise NotImplementedError
+
+    @property
+    def data(self):
+        if self['ResultFormat'] == 'JSON':
+            tmp_data = self._json_data
+        else:
+            tmp_data = self._xml_data
+        return tmp_data
+
+
+class RegionalDataRequest(DataRequest):
 
     def __init__(self, UserID, KeyCode, ResultFormat='JSON', **params):
         required_params = {'UserID': UserID,
@@ -75,7 +146,7 @@ class RegionalDataRequest(Request):
         super(RegionalDataRequest, self).__init__(**required_params)
 
 
-class NIPARequest(Request):
+class NIPARequest(DataRequest):
 
     def __init__(self, UserID, TableID, Frequency, Year, ResultFormat='JSON',
                  **params):
@@ -90,7 +161,7 @@ class NIPARequest(Request):
         super(NIPARequest, self).__init__(**required_params)
 
 
-class NIUnderlyingDetailRequest(Request):
+class NIUnderlyingDetailRequest(DataRequest):
 
     def __init__(self, UserID, TableID, Frequency, Year, ResultFormat='JSON',
                  **params):
@@ -105,7 +176,7 @@ class NIUnderlyingDetailRequest(Request):
         super(NIUnderlyingDetailRequest, self).__init__(**required_params)
 
 
-class FixedAssetsRequest(Request):
+class FixedAssetsRequest(DataRequest):
 
     def __init__(self, UserID, TableID, Year, ResultFormat='JSON', **params):
         required_params = {'UserID': UserID,
