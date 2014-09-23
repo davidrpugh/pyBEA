b22b95842037959767da71cc65cb3b89408d4d9c
David R. Pugh <drobert.pugh@gmail.com>
2014-09-22 21:18:48 -0400

    Added some tests for pybea.py

--- pybea/tests/test_pybea.py
+++ pybea/tests/test_pybea.py
@@ -6,6 +6,32 @@ Tests for pybea.py module.
 
 """
 import nose
-import unittest
+import pandas as pd
 
 from .. import pybea
+
+
+def test_get_data_set_list():
+    """Testing function for grabbing list of available data sets."""
+    df = pybea.get_data_set_list()
+    nose.tools.assert_is_instance(df, pd.DataFrame)
+    nose.tools.assert_equals(df.shape, (4, 2))
+
+
+def test_get_parameter_list():
+    """Testing function for grabbing list of parameters for a data set."""
+    df = pybea.get_parameter_list(DataSetName='FixedAssets')
+    nose.tools.assert_is_instance(df, pd.DataFrame)
+    nose.tools.assert_equals(df.shape, (2, 6))
+
+
+def test_get_parameter_values():
+    """Testing function for grabbing list of values for a parameter."""
+    df = pybea.get_parameter_values(DataSetName='RegionalData',
+                                    ParameterName='GeoFips')
+    nose.tools.assert_is_instance(df, pd.DataFrame)
+    nose.tools.assert_equals(df.shape, (2, 6))
+
+
+def test_get_data_set():
+    pass
