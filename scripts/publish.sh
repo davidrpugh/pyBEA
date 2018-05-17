599e2d22b5184205a07d573b061fa2ec1f7f4b33
David R. Pugh <drobert.pugh@gmail.com>
2018-05-17 09:23:59 +0300

    Added --repository-url tory and fix permissions

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,7 @@ set -ev  # Needed in order for Travis CI to report failure
 if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_EVENT_TYPE == "push" ]
 then
   python setup.py sdist bdist_wheel
-  twine upload  -u $PYPI_USERNAME -p $PYPI_PASSWORD dist/*
+  twine upload  --repository-url https://pypi.python.org/pypi -u $PYPI_USERNAME -p $PYPI_PASSWORD dist/*
 else
   echo "No releases published for this build!"
 fi
